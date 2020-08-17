c58bb7af0fa1eccca0a5900b8bd0eed2e1fc1878
Mark Zieg <mark@zieg.com>
2020-08-17 11:27:31 -0400

    killed Login

--- SpectralCalculator/AppShell.xaml.cs
+++ SpectralCalculator/AppShell.xaml.cs
@@ -11,9 +11,6 @@ namespace SpectralCalculator
         public AppShell()
         {
             InitializeComponent();
-            // Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            // Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
-
     }
 }
