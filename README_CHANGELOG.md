8ace4c0cb9bb24d1d659c5b94820e53c04cbf561
Mark Zieg <mark@zieg.com>
2020-08-17 15:36:49 -0400

    both screens working, equations, logo, icon

--- README_CHANGELOG.md
+++ README_CHANGELOG.md
@@ -2,6 +2,10 @@
 
 - 2020-08-17 0.0.2
     - allow entry of negative wavenumbers on iOS
+    - added Range screen
+    - added equations to Peak screen
+    - added logo to both screens
+    - added iOS appicon
 - 2020-08-14 0.0.1
     - created fresh Xamarin.Shell template
     - got basic Peak screen working
--- SpectralCalculator.Android/Resources/Resource.designer.cs
+++ SpectralCalculator.Android/Resources/Resource.designer.cs
@@ -8373,14 +8373,23 @@ namespace SpectralCalculator.Droid
 			// aapt resource value: 0x7F070075
 			public const int tab_feed = 2131165301;
 			
+			// aapt resource value: 0x7F070078
+			public const int tooltip_frame_dark = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int tooltip_frame_light = 2131165305;
+			
 			// aapt resource value: 0x7F070076
-			public const int tooltip_frame_dark = 2131165302;
+			public const int to_wavelength = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_light = 2131165303;
+			public const int to_wavenumber = 2131165303;
 			
-			// aapt resource value: 0x7F070078
-			public const int xamarin_logo = 2131165304;
+			// aapt resource value: 0x7F07007A
+			public const int wp_logo = 2131165306;
+			
+			// aapt resource value: 0x7F07007B
+			public const int xamarin_logo = 2131165307;
 			
 			static Drawable()
 			{
